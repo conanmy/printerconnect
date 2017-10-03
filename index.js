6e01c6a61d7087145555c05a5fce2427933bda8c
Frank Ma <Frank.Ma@reckon.com>
2017-10-03 16:13:03 +1300

    first stable

--- index.js
+++ index.js
@@ -19,12 +19,8 @@ app.all('/print', function (req, res) {
     .style('bu')
     .size(1, 1)
     .text('The quick brown fox jumps over the lazy dog')
-    .text('敏捷的棕色狐狸跳过懒狗')
-    .barcode('1234567', 'EAN8')
-    .qrimage('https://github.com/song940/node-escpos', function(err){
-      this.cut();
-      this.close();
-    });
+    .cut()
+    .cashdraw(5);
   });
   console.log('done');
   console.log(device);
